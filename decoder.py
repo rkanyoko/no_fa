cb1aa6df90c88c4b10f5c0c591b04079e8f71593
kanyoko <reubenkanyoko25@gmail.com>
2026-03-26 03:14:33 +0300

    decode and decompress the cookie

---
+++ decoder.py
@@ -0,0 +1,13 @@
+import zlib
+import base64
+
+#Load up the cookie you get from the website after you login
+#keep in mind that OTP codes expire after some time therefore be sure to copy the latest cookie for this to work
+cookie_data = ".eJwty0sKgCAQANC7zFoCyx9eJoacRHBU1FbR3WvR9sG7IdcYKYCHE_MgEFBn2wcdneaHzsntt5mYxkRu4KW1SrnVGLsoI5U2WsA1qBdk-hIGTgWeF0d2HGo.acR06w.xeGBwzLq-hPsU3pBVt_QAxrFyGE"
+
+# decoding with base64 and decompress with zlib
+decoded = base64.urlsafe_b64decode(cookie_data + "==")
+decompressed = zlib.decompress(decoded)
+
+#we now print the final result, this should have averything in plaintext therefore you can see the OTP code for the specific user.
+print(decompressed.decode())
